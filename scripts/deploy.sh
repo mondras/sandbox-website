57a5bed30a3269a9a35314535e0e583cf3e69030
Carlos Mondragon <cadragon@gmail.com>
2019-10-06 11:14:05 -0700

    Build site on push using github actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,3 @@ mkdir -p docs/assets
 mkdir -p docs/js
 echo 'sandbox.is' > docs/CNAME
 NODE_ENV=production webpack -p --progress
-git add *
-git add -f docs/*
-git commit -am 'Automated website edits deployment'
-git push
